9e58cfef93900d2ee74ba636b622bee96202490a
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-05 20:41:06 +0100

    Add explanation of data types in javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+// Data Types
+
+// There are 8 data types in javascript:
+// string
+// number
+// bigint
+// boolean
+// undefined
+// null
+// symbol
+// object (objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, etc)
+
+// A javascript variable can hold any type of data
+// Javascript has "dynamic types", meaning the same variable can hold different data types
